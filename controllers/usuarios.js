3b4c18ee278d2901d7dd418a99bcd2251fbf21db
Angel Travieso <angeltraviesoc@gmail.com>
2023-01-20 14:01:18 -0400

    POST: creando un usuario en la coleccion

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,11 @@
 // Para obtener el tipado
-const { response, request } = require('express');
+const {
+    response,
+    request
+} = require('express');
+
+// en mayuscula para crear nuevas instancias del modelo
+const Usuario = require('../models/usuario');
 
 
 /*
@@ -20,7 +26,13 @@ const usuariosGet = (req = request, res = response) => {
     // const params = req.query;
 
     // Lo que necesito
-    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+    const {
+        q,
+        nombre = 'No name',
+        apikey,
+        page = 1,
+        limit
+    } = req.query;
 
     res.json({
         msg: 'get API - usuariosGet',
@@ -32,15 +44,33 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPost = (req = request, res = response) => {
+const usuariosPost = async (req, res = response) => {
 
-    // const body = req.body;
-    const { nombre, edad } = req.body;
+    // const {
+    //     nombre,
+    //     edad
+    // } = req.body;
+    const body = req.body;
 
-    res.status(201).json({
-        msg: 'post API - usuariosPost',
-        nombre,
-        edad
+    /*
+    lo que no esté incluido en mi modelo y venga en la peticion
+    mongoose lo ignorará y no lo insertara en la coleccion de mongo
+    */
+
+    // instanciar modelo
+    const usuario = new Usuario(body);
+
+    // para guardar la coleccion (por defecto no lo guarda)
+    await usuario.save();
+
+    // cambiar codigo HTTP de la respuesta
+    // res.status(201).json({
+    //     msg: 'post API - usuariosPost',
+    // });
+
+    res.json({
+        // msg: 'post API - usuariosPost',
+        usuario
     });
 }
 
@@ -49,7 +79,9 @@ const usuariosPut = (req = request, res = response) => {
     // const id = req.params.id; (http://localhost:8082/api/usuarios/10)
 
     // Extraer lo que necesito
-    const { id } = req.params;
+    const {
+        id
+    } = req.params;
 
     res.status(401).json({
         msg: 'put API - usuariosPut',
