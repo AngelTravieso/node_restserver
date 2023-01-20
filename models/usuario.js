08bbeeed6387fda42a185d05e89bd29baea875ca
Angel Travieso <angeltraviesoc@gmail.com>
2023-01-20 13:44:21 -0400

    modelo de usuario

---
+++ models/usuario.js
@@ -0,0 +1,42 @@
+const {
+    Schema,
+    model
+} = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true // solo registros unicos (no duplicados)
+    },
+    password: {
+        type: String,
+
+    },
+    img: {
+        type: String,
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    state: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+
+/*
+mongoose lo vuelve en plural y crea la coleccion usuarios
+*/
+module.exports = model('Usuario', UsuarioSchema);
