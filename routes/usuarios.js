f5dc3d79ea161649b778458b5cae28d0020e6d56
AngelTravieso <angeltraviesoc@gmail.com>
2023-01-21 13:47:33 -0400

    validar rol contra la BD

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 const { check } = require('express-validator');
+const Role = require('../models/rol');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -32,13 +33,30 @@ Crea un middleware para validar uno o mas campos de la peticion entrante
 // ], usuariosPost);
 
 
-router.post('/',[
+router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
     check('email', 'El correo no es válido').isEmail(),
-    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+
+    /*
+        1. custom() para pasar funcion personalizada y validar un parametro de la request
+        2. rol = '' : es el valor 'rol' que me llega en la peticion (request), si no llega nada que sea String vacio
+        3. busco en mi colecciòn de 'Roles' y verifico que el rol que llega en la peticiòn este guardado en mi colecciòn de 'Roles'
+        4. muestro error personalizado (no revienta la app) 
+    */
+
+    check('rol').custom(async (rol = '') => {
+        const existeRol = await Role.findOne({ rol });
+
+        if (!existeRol) {
+            // lanzar error personalizado (no revienta la app)
+            throw new Error(`El rol ${rol} no está registrado en la BD`);
+        }
+
+    }),
     validarCampos
-], usuariosPost );
+], usuariosPost);
 
 router.put('/', usuariosPut);
 
