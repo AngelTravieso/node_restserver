df8bc7c1bbfc0ff4635f39d20dec2079f78270c5
AngelTravieso <angeltraviesoc@gmail.com>
2023-01-21 23:05:50 -0400

    login usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,53 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
+const Usuario = require('../models/usuario');
 
-    
+const login = async (req, res = response) => {
 
-    res.json({
-        msg: 'login ok',
-    })
+    const { email, password } = req.body;
+
+    try {
+
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ email });
+
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - email',
+            });
+        }
+
+        // Verificar si el usuario está activo
+        if (!usuario.state) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - state: false',
+            });
+        }
+
+        // Verificar la contraseña
+
+        // Compara el password que llega con el guardado en la BD
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password',
+            });
+        }
+
+        // Generar el JWT
+
+        res.json({
+            msg: 'login ok',
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+
 }
 
 module.exports = {
