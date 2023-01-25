a458bfff4881b773ab64d5d0ee959a6d3bc3fc50
Angel Travieso <angeltraviesoc@gmail.com>
2023-01-25 13:27:58 -0400

    busquedas en base de datos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,13 +1,64 @@
 const { response } = require("express");
+const { ObjectId } = require('mongoose').Types;
+
+const { Usuario } = require('../models');
+
+const coleccionesPermitidas = [
+    'categoria',
+    'productos',
+    'rol',
+    'usuarios',
+];
+
+const buscarUsuarios = async( termino = '', res = response ) => {
+
+    // Validar si el termino es un ID de mongo válido
+    const esMongoID = ObjectId.isValid( termino );
+
+    // Si es un ID válido
+    if(esMongoID) {
+        const usuario = await Usuario.findById( termino );
+        return res.json({
+            // Si el usuario existe regreso un arreglo con el usuario, sino un array vacio
+            results: ( usuario ) ? [ usuario ] : [],
+        });
+    }
+
+}
 
 const buscar = (req, res = response) => {
 
     // Obtener parametros de busqueda
-    const { coleccion, termino } = req.params;
+    const {
+        coleccion,
+        termino
+    } = req.params;
 
-    res.json({
-        coleccion, termino,
-    });
+    // Validar la coleccion por la que se busca
+    if (!coleccionesPermitidas.includes(coleccion)) {
+        return res.status(400).json({
+            msg: `Las coleciones permitidas son ${coleccionesPermitidas}`,
+        });
+    }
+
+    switch (coleccion) {
+        case 'categoria':
+            break;
+        case 'productos':
+            break;
+        case 'usuarios':
+            buscarUsuarios(termino, res)
+            break;
+        default:
+            res.status(500).json({
+                msg: 'Se me olvidó haceer esta búsqueda',
+            });
+    }
+
+    // res.json({
+    //     coleccion,
+    //     termino,
+    // });
 
 }
 
