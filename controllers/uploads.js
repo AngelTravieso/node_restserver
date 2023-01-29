0ea131733f5feda92c3fac603048ea53ca77c9f9
AngelTravieso <angeltraviesoc@gmail.com>
2023-01-28 20:16:35 -0400

    actualizar imagen de usuario y producto

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 
 const { subirArchivo } = require('../helpers');
+const { Usuario, Producto } = require('../models');
 
 // Cargar archivos
 const cargarArchivo = async (req, res = response) => {
@@ -23,15 +24,52 @@ const cargarArchivo = async (req, res = response) => {
 }
 
 // Actualiar img
-const actualizarImagen = async (req, res = response ) => {
+const actualizarImagen = async (req, res = response) => {
 
-    const { coleccion, id} = req.params;
+    const { coleccion, id } = req.params;
 
-    res.json({
-        coleccion,
-        id,
-    });
+    let modelo;
 
+    switch (coleccion) {
+        case 'usuarios':
+
+            // Verificar si la coleccion tiene el ID que me llega
+            modelo = await Usuario.findById(id);
+
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`,
+                });
+            }
+
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`,
+                });
+            }
+
+            break;
+        default:
+            return res.status(500).json({
+                msg: 'Se me olvidó validar esto'
+            });
+    }
+
+    // Subir archivo (img) en carpeta (coleccion)
+    const nombre = await subirArchivo(req.files, undefined, coleccion);
+
+    // Agregar la img al modelo
+    modelo.img = nombre;
+
+    // Guardar nuevo modelo (puede ser usuario o productos)
+    await modelo.save();
+
+    // Retornar modelo
+    res.json(modelo);
 
 }
 
